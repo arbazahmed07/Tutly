8e8ff71be55ae21a99d2e65bea57509d9f2bebd3
Uday Sagar <udaysagar.mail@gmail.com>
2024-04-18 19:16:26 +0530

    Fix playground ui

---
+++ src/app/playground/html-css-js/_components/Playground.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React from 'react'
+import Split from 'react-split';
+import "@/styles/react-split.css"
+
+import { defaultState } from './config';
+import { Context } from './context';
+
+import { useState } from "react";
+import PreviewPanel from './PreviewPanel';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import EditorPanel from './EditorPanel';
+const Playground = () => {
+
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  const [state, dispatch] = useLocalStorage('html-css-js-playground-state', defaultState);
+  const [theme, setTheme] = useLocalStorage('editor-theme', 'light');
+
+  return (
+    <Context.Provider value={{ state, dispatch, currentTabIndex, setCurrentTabIndex, theme, setTheme }}>
+      <Split
+        className="flex h-[90vh] overflow-hidden mb-2"
+        sizes={[50, 50]}
+        minSize={300}
+      >
+        <div className='h-full py-2 mx-1 relative'>
+          <EditorPanel />
+        </div>
+        <PreviewPanel />
+      </Split>
+    </Context.Provider>
+
+  )
+}
+
+export default Playground
--- src/app/playground/html-css-js/page.tsx
+++ src/app/playground/html-css-js/page.tsx
@@ -1,42 +1,14 @@
-"use client"
-
 import React from 'react'
-import Split from 'react-split';
-import "@/styles/react-split.css"
-
-import { defaultState } from './_components/config';
-import { Context } from './_components/context';
-
-import { useState } from "react";
-import PreviewPanel from './_components/PreviewPanel';
-import useLocalStorage from '@/hooks/useLocalStorage';
 import dynamic from 'next/dynamic'
 
-const EditorPanel = dynamic(() => import('./_components/EditorPanel'), {
+const Playground = dynamic(() => import('./_components/Playground'), {
   ssr: false,
 })
 
-const Page = () => {
-
-  const [currentTabIndex, setCurrentTabIndex] = useState(0);
-  const [state, dispatch] = useLocalStorage('html-css-js-playground-state', defaultState);
-  const [theme, setTheme] = useLocalStorage('editor-theme', 'light');
-
+const page = () => {
   return (
-    <Context.Provider value={{ state, dispatch, currentTabIndex, setCurrentTabIndex, theme, setTheme }}>
-      <Split
-        className="flex h-[90vh] overflow-hidden mb-2"
-        sizes={[50, 50]}
-        minSize={300}
-      >
-        <div className='h-full py-2 mx-1 relative'>
-          <EditorPanel />
-        </div>
-        <PreviewPanel />
-      </Split>
-    </Context.Provider>
-
+    <Playground />
   )
 }
 
-export default Page
+export default page
