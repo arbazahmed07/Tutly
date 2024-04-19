0c07d131f65573dddc958bccdc5ac1281cb65b9c
vighnesh-7 <vangarivighnesh@gmail.com>
2024-04-20 02:38:35 +0530

    add attachment

--- src/app/(dashboard)/courses/[id]/class/[classId]/page.tsx
+++ src/app/(dashboard)/courses/[id]/class/[classId]/page.tsx
@@ -1,16 +1,42 @@
+
 import { getClassDetails } from "@/actions/courses"
+import getCurrentUser from "@/actions/getCurrentUser";
 import YoutubeEmbed from "@/components/YoutubeEmbed";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Class({ params }: { params: { classId: string } }) {
     const details = await getClassDetails(params.classId);
+    const currentUser = await getCurrentUser();
+
     return (
         <div className="w-full flex justify-between m-8">
-            <div>
-                <YoutubeEmbed embedId="Big_aFLmekI" />
-                <div className="mt-2 rounded bg-secondary-500 p-4">
-                    Attachments
+            {
+                currentUser?.role === "STUDENT" ? (
+                <div>
+                    <YoutubeEmbed embedId="Big_aFLmekI" />
+                    <div className="mt-2 rounded bg-secondary-500 p-4">
+                        Attachments
+                        {currentUser?.role }
+                    {/* <div className="w-[400px] rounded bg-secondary-600 p-4">Doubts | Timestamps</div> */}
+                    </div>
                 </div>
-            </div>
+                ) : (
+                <div className=" ">
+                    <YoutubeEmbed embedId="Big_aFLmekI" />
+                    <div className=" flex justify-between items-center mx-2 mt-4">
+                        <div className="mt-2 rounded bg-secondary-500 p-4">
+                            Attachments
+                        </div>
+                        <Link href={'/assignments/new'} >
+                            <Button variant={'secondary'} size={'sm'}>
+                                Add Attachment
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+                )
+            }
             <div className="w-[400px] rounded bg-secondary-600 p-4">Doubts | Timestamps</div>
         </div>
     )
---
+++ src/hooks/currentUser.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+const useTimeStore = create((set) => ({
+    setUser : '',
+    getUser : '',
+    // setMode : (Mode) =>set({Mode}),
+    // setStartTime : () => set({ startTime: new Date().toISOString() }),
+    // setEndTime : () => set({ endTime: new Date().toISOString() }),
+    // clear : () => set({ startTime: 0,endTime:0}),
+    
+}));
+
+export default currentUser;
