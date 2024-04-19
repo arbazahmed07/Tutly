19c27d859a7ed6ed087d7003bce1b4a9feeeadc2
Uday Sagar <udaysagar.mail@gmail.com>
2024-04-19 20:46:14 +0530

    middleware pending ?

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const NEXT_PUBLIC_SIGN_IN_URL = "/api/auth/signin";
-export const NEXT_PUBLIC_SIGN_UP_URL = "/api/auth/signup";
+export const NEXT_PUBLIC_SIGN_IN_URL = "/signin";
+export const NEXT_PUBLIC_SIGN_UP_URL = "/signup";
 export const NEXT_PUBLIC_AFTER_SIGN_IN_URL = "/";
 export const NEXT_PUBLIC_AFTER_SIGN_UP_URL = "/";
