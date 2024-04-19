6b9fd29b6e17f01005b23f8a4423b4694eb9bb82
goutham4126 <goutham4126@gmail.com>
2024-04-19 18:56:19 +0530

    signin ui updated

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+    
     ],
   },
 };
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,11 @@
 import { FC, ReactNode } from 'react';
-
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
-  return <div className='flex h-screen w-full justify-center items-center '>
-    <div className='p-7 m-2 md:p-10 sm:w-[400px] bg-slate-200 rounded-lg shadow-lg'>
+  return <div className='flex h-screen w-full justify-center bg-primary-400 items-center'>
+    <div className='p-7 m-2 md:p-10 sm:w-[400px] rounded-lg bg-secondary-50 shadow-[0_20px_20px_20px_rgba(0,0,0,0.1)]'>
       {children}
     </div>
   </div>;
