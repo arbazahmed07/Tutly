243bace8dd961e4b1e711f41a8823f73a0ba4aea
kalyan <kalyantingani@gmail.com>
2024-04-20 23:56:24 +0530

    leaderboard

--- src/app/(dashboard)/leaderboard/page.tsx
+++ src/app/(dashboard)/leaderboard/page.tsx
@@ -1,26 +1,16 @@
+import { getEnrolledCourses } from "@/actions/courses";
 import getLeaderboardData from "@/actions/getLeaderboard";
-import { useRouter } from "next/router";
-import StudentTable from "./leaderboard";
+import Leaderboard from "@/components/leaderBoard";
 
+export default async function Page() {
+  const assignments = await getLeaderboardData();
+  const courses = await getEnrolledCourses();
 
-
-
-export default async function Leaderboard() {
-
-    const points: any = await getLeaderboardData();
-    
-    return (
-        <div>Leaderboard
-
-            <pre>
-                {JSON.stringify(points, null, 2)}
-            </pre>
-
-            <div>
-                start here
-                {/* <LeaderboardComponent submissions={points} /> */}
-                <StudentTable submissions={points} />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <Leaderboard assignments={assignments} courses={courses}/>
+      </div>
+    </div>
+  );
 }
