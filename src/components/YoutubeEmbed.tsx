91c024e238b567551984dc65aa73500a7c203d8d
kalyan <kalyantingani@gmail.com>
2024-04-20 20:58:32 +0530

    sidebar-responsive-2

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -4,9 +4,10 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId } :{
     embedId : string
 }) => (
-  <div className="w-full">
+  <div className="">
     <iframe
-      width="80%"
+      width="853"
+      height="480"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
