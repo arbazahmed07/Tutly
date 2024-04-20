f6a2828c1751a6c516878d53a0f074e799187c9f
kalyan <kalyantingani@gmail.com>
2024-04-20 16:01:54 +0530

    half-way-to-assignments-page

--- src/app/(dashboard)/assignments/page.tsx
+++ src/app/(dashboard)/assignments/page.tsx
@@ -1,28 +1,15 @@
-import { getAllAssignedAssignments } from "@/actions/getAssignments"
+import { getEnrolledCourses } from "@/actions/courses";
+import { getAllAssignedAssignments } from "@/actions/getAssignments";
+import AssignmentBoard from "@/components/assignmentBoard";
 import Link from "next/link";
 
 export default async function Assignments() {
-
-    const assignments = await getAllAssignedAssignments();
-
-    return (
-        <div>
-            Assignments
-
-            {
-                assignments?.map((assignment) => (
-                    <div key={assignment.id} className="flex gap-3 items-center">
-                        <div>
-                            {assignment.assignment.title}-{assignment.assignment.id}
-                        </div>
-                        <Link href={`/assignments/${assignment.id}`} className="p-2 bg-secondary-800 rounded-md">
-                            View Details / Submit
-                        </Link>
-                    </div>
-                ))
-            }
-
-            <pre>{JSON.stringify(assignments, null, 2)}</pre>
-        </div >
-    )
+  const assignments = await getAllAssignedAssignments();
+  const courses = await getEnrolledCourses();
+  return (
+    <div className="mx-14 px-8 py-2 border flex flex-col gap-4">
+      <h1 className="text-center text-xl font-bold border py-2">ASSIGNMENTS</h1>
+      <AssignmentBoard courses={courses} assignments={assignments} />
+    </div>
+  );
 }
