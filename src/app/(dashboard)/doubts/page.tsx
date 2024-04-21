421103d995c21080ebb74739fa2638a1a0a495a0
Uday Sagar <udaysagar.mail@gmail.com>
2024-04-21 16:07:16 +0530

    fixed styling of comming soon

--- src/app/(dashboard)/doubts/page.tsx
+++ src/app/(dashboard)/doubts/page.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import "../../../styles/globals.css"
 const page = () => {
   return (
-    <div className="w-full h-[90vh] bg-primary-800 z-50 flex justify-center items-center">
-      <div className="relative w-full h-20 flex items-center justify-center loading-text">
-        <span className="inline-block  mr-5">C</span>
-        <span className="inline-block  mr-5">O</span>
-        <span className="inline-block  mr-5">M</span>
-        <span className="inline-block  mr-5">I</span>
-        <span className="inline-block  mr-5">N</span>
-        <span className="inline-block  mr-10">G</span>
-        <span className="inline-block  mr-5">S</span>
-        <span className="inline-block  mr-5">O</span>
-        <span className="inline-block  mr-5">O</span>
-        <span className="inline-block  mr-5">N</span>
+    <div className="w-full h-[90vh] bg-background text-foreground z-50 flex justify-center items-center">
+      <div className="relative w-full h-20 flex items-center justify-center loading-text text-2xl">
+        <span>C</span>
+        <span>O</span>
+        <span>M</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+        <span></span>
+        <span>S</span>
+        <span>O</span>
+        <span>O</span>
+        <span>N</span>
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
       </div>
     </div>
   );
