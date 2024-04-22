d4f94313984a7f9c854051ef3281c523605400c4
goutham4126 <goutham4126@gmail.com>
2024-04-22 22:12:09 +0530

    courses ui updated

--- src/components/videoEmbeds/DriveEmbed.tsx
+++ src/components/videoEmbeds/DriveEmbed.tsx
@@ -7,11 +7,12 @@ const DriveEmbed = ({ embedId }: {
   <div className="">
     <iframe
       width="100%"
-      height="480"
+      height="100%"
       src={`https://drive.google.com/file/d/${embedId}/preview`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded Drive"
+      className="md:h-[450px]"
     />
   </div>
 );
