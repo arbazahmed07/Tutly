9488a82f41d80c6cc321f94400c44a0ced7e9e7f
Uday Sagar <udaysagar.mail@gmail.com>
2024-04-23 23:44:18 +0530

    fix merge

--- src/components/layouts/home-layout.tsx
+++ src/components/layouts/home-layout.tsx
@@ -7,7 +7,7 @@ import { MdAirplay, MdOutlineAssignment } from "react-icons/md";
 import { MdOutlineLeaderboard } from "react-icons/md";
 import { TbMessageQuestion } from "react-icons/tb";
 import { FaChalkboardTeacher } from "react-icons/fa";
-import { Suspense,  useState } from "react";
+import { Suspense, useState } from "react";
 import Loading from "@/app/(dashboard)/loading";
 import { usePathname } from "next/navigation";
 
@@ -68,8 +68,8 @@ export default function HomeLayout({
         name: "Courses",
         icon: <FaChalkboardTeacher />,
         path: "/courses"
-    },
-    {
+      },
+      {
         name: "Assignments",
         icon: <MdOutlineAssignment />,
         path: "/assignments",
@@ -88,27 +88,20 @@ export default function HomeLayout({
         name: "Playgrounds",
         icon: <MdAirplay />,
         path: "/playground/html-css-js"
-    }
-]
-
-export default function HomeLayout({ children, currentUser }: {
-    children: React.ReactNode,
-    currentUser: any
-}) {
-    const [menu, setMenu] = useState<boolean>(true);
-    const pathname = usePathname();
-    const isCoursePage = pathname.startsWith('/courses/');
-    return (
-        <div className="w-full">
-            <Navbar currentUser={currentUser} menu={menu} setMenu={setMenu} />
-            <div className="flex">
-                {!isCoursePage&&<Sidebar items={items} menu={menu} setMenu={setMenu}/>}
-                <Suspense fallback={<Loading />}>
-                    <div className="w-full">
-                    {children}
-                    </div>
-                </Suspense>
-            </div>
-        </div>
-    )
+      }
+    ]
+  }
+  return (
+    <div className="w-full">
+      <Navbar currentUser={currentUser} menu={menu} setMenu={setMenu} />
+      <div className="flex">
+        {!isCoursePage && <Sidebar items={items} menu={menu} setMenu={setMenu} />}
+        <Suspense fallback={<Loading />}>
+          <div className="w-full">
+            {children}
+          </div>
+        </Suspense>
+      </div>
+    </div>
+  )
 }
