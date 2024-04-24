ab4ee0d1e570e24e8b65cd9995b72395452beb20
goutham4126 <goutham4126@gmail.com>
2024-04-25 03:08:13 +0530

    students ui changed

--- src/app/mentor/assignments/page.tsx
+++ src/app/mentor/assignments/page.tsx
@@ -9,13 +9,21 @@ export default async function mentorAssignments(a: any) {
 
   return (
     <div className="mx-14 px-8 py-2 flex flex-col gap-4">
-      <h1 className="text-center text-xl font-bold border py-2">Students</h1>
+      <div>
+      <h1 className="text-center text-xl bg-gradient-to-r from-blue-600 to-sky-500 font-semibold rounded-lg py-2">Students</h1>
+      {/* <h1>Search bar comes here</h1> */}
+      </div>
       {
         students && students.map((student, index) => {
           return (
-            <div key={index}>
-              <h1>{student.name}</h1>
-              <Link href={`./assignments/${student.id}`}>Link</Link>
+            <div key={index} className={`${index<students.length-1&&"border-b pb-3"}`}>
+                <div className="p-2 flex justify-between items-center">
+                  <div>
+                    <h1>{student.name}</h1>
+                    <h1 className="text-sm font-medium">{student.username}</h1>
+                  </div>
+                  <Link href={`./assignments/${student.id}`} className="bg-slate-500 p-2 rounded-lg">Assignments</Link>
+                </div>
             </div>
           )
         })
