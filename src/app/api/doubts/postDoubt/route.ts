a51a905be212fabf5fb56e8ecea769a5e9022e47
Kalyan Tingani <kalyantingani@gmail.com>
2024-04-25 03:51:46 +0530

    community portal compelted

--- src/app/api/doubts/postDoubt/route.ts
+++ src/app/api/doubts/postDoubt/route.ts
@@ -2,10 +2,10 @@ import { createDoubt } from "@/actions/doubts";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const { classId, title, description } = await request.json();
-
+  const { courseId, title, description } = await request.json();
+  
   try {
-    const doubt = await createDoubt(classId, title, description);
+    const doubt = await createDoubt(courseId, title, description);
     return NextResponse.json(doubt);
   } catch (e: any) {
     return NextResponse.json({ error: e.message }, { status: 400 });
