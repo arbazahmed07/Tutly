ff6fbfc3114c30f0a11632145fb0ef22e0505178
goutham4126 <goutham4126@gmail.com>
2024-04-25 03:25:12 +0530

    mentors assignment ui changed

--- src/app/mentor/assignments/page.tsx
+++ src/app/mentor/assignments/page.tsx
@@ -1,6 +1,6 @@
 import { getMentorStudents } from "@/actions/courses";
 import Link from "next/link";
-
+import Image from "next/image";
 
 export default async function mentorAssignments() {
   const students = await getMentorStudents();
@@ -13,15 +13,18 @@ export default async function mentorAssignments() {
       {/* <h1>Search bar comes here</h1> */}
       </div>
       {
-        students && students.map((student, index) => {
+        students && students.map((student:any, index:number) => {
           return (
             <div key={index} className={`${index<students.length-1&&"border-b pb-3"}`}>
-                <div className="p-2 flex justify-between items-center">
-                  <div>
-                    <h1>{student.name}</h1>
-                    <h1 className="text-sm font-medium">{student.username}</h1>
+                <div className="p-1 flex justify-between items-center">
+                  <div className="flex gap-5 items-center">
+                    <Image src={student.image} height={40} width={40} alt="" className="rounded-full"/>
+                    <div>
+                      <h1 className="text-sm font-medium">{student.name}</h1>
+                      <h1 className="text-xs font-medium">{student.username}</h1>
+                    </div>
                   </div>
-                  <Link href={`./assignments/${student.id}`} className="bg-slate-500 p-2 rounded-lg">Assignments</Link>
+                  <Link href={`./assignments/${student.id}`} className="bg-slate-500 p-2 text-sm font-medium rounded-lg">Assignments</Link>
                 </div>
             </div>
           )
