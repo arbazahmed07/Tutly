5260e4994d80bb17b080e6e28b207fb47a6d4de4
Uday Sagar <udaysagar.mail@gmail.com>
2024-04-26 03:57:37 +0530

    coursepage fix

--- src/app/mentor/assignments/page.tsx
+++ src/app/mentor/assignments/page.tsx
@@ -4,15 +4,20 @@ import MentorAssignmentBoard from "@/components/mentorAssignmentBoard";
 export default async function mentorAssignments() {
   const students = await getMentorStudents();
   const courses = await getMentorCourses();
-  if (!students) return;
+  if(!courses || !students ) return <div className="text-center">Sign in to view assignments!</div>
 
   return (
     <div className="mx-14 px-8 py-2 flex flex-col gap-4">
       <div>
       <h1 className="text-center text-xl bg-gradient-to-r from-blue-600 to-sky-500 font-semibold rounded-lg py-2">Students</h1>
-      {/* <h1>Search bar comes here</h1> */}
+      {
+        courses && courses.length > 0 ? (
+          <MentorAssignmentBoard students={students} courses={courses}/>
+        ) : (
+          <div className="text-center">No courses found!</div>
+        )
+      }
       </div>
-      <MentorAssignmentBoard students={students} courses={courses}/>
     </div>
   );
 }
