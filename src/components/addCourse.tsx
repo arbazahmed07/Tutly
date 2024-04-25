756b964f2d376017d625ea1227532b93eb727e14
vighnesh-7 <vangarivighnesh@gmail.com>
2024-04-26 04:43:20 +0530

    add course

--- src/components/addCourse.tsx
+++ src/components/addCourse.tsx
@@ -1,60 +1,111 @@
 "use client";
 import { createCourse } from "@/actions/courses";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { IoMdCloseCircle } from "react-icons/io";
 
 function AddCourse() {
   const [openPopup, setOpenPopup] = useState<boolean>(false);
   const [courseTitle, setCourseTitle] = useState<string>("");
-    const handleSubmit = async () => {
-    const newCourse = await axios.post("/api/course/create",{
-    title:courseTitle,
-    isPublished:true,
-    })
-      setOpenPopup(!openPopup);
+  const [img, setImg] = useState<string>("");
+  const [isPublished, setIsPublished] = useState<boolean>(false);
+  const router = useRouter()
+
+  const handleSubmit = async () => {
+    const res = await axios.post("/api/course/create",{
+          title:courseTitle,
+          isPublished:isPublished,
+          image : img
+        })
+          setOpenPopup(!openPopup);
+
+    if (res.data.error) {
+        toast.error("Failed to add newClass");
+    } else {
+        toast.success("newClass added successfully");
+        router.refresh()
+    }
     };
-  return (
-    <>
-      <div
-        onClick={() => setOpenPopup(!openPopup)}
-        className="rounded-lg border cursor-pointer m-5 flex flex-col items-center justify-center"
-        style={{
-          boxShadow:
-            "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
-        }}
-      >
-        <div className="text-center">
-          <IoIosAddCircleOutline className="text-6xl" />
-          <h1>Add</h1>
-        </div>
-      </div>
-      {openPopup && (
-        <div className="absolute top-[200px] left-[45%] min-w-[300px] bg-secondary-500 p-4 rounded-lg">
+        
+      return (
+        <>
           <div
             onClick={() => setOpenPopup(!openPopup)}
-            className="absolute right-2 top-2 cursor-pointer text-md"
+            className="rounded-lg border cursor-pointer m-5 flex flex-col items-center justify-center"
+            style={{
+              boxShadow:
+                "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
+            }}
           >
-            <IoMdCloseCircle />
+            <div className="text-center">
+              <IoIosAddCircleOutline className="text-6xl" />
+              <h1>Add</h1>
+            </div>
           </div>
-          <h1 className="text-md text-center my-4">ADD NEW COURSE</h1>
-          <input
-            onChange={(e) => setCourseTitle(e.target.value)}
-            type="text"
-            className="rounded p-2 bg-background block m-auto w-full"
-            placeholder="Title"
-          />
-          <button
-            onClick={handleSubmit}
-            className="rounded bg-primary-500 p-2 block m-auto my-3 w-full"
-          >
-            Create
-          </button>
-        </div>
-      )}
-    </>
-  );
+          {openPopup && (
+            <div className="absolute top-[200px] left-[45%] min-w-[300px] space-y-5 bg-secondary-500 p-4 rounded-lg">
+              <div
+                onClick={() => setOpenPopup(!openPopup)}
+                className="absolute right-2 top-2 cursor-pointer text-md"
+              >
+                <IoMdCloseCircle />
+              </div>
+              <div className="mb-4">
+                <h1 className="text-md text-center my-4">ADD NEW COURSE</h1>
+                <input
+                  onChange={(e) => setCourseTitle(e.target.value)}
+                  type="text"
+                  className="rounded p-2 bg-background block m-auto w-full mb-4"
+                  placeholder="Title"
+                  />
+              </div>
+              <label className="" htmlFor="publish">Publish:</label>
+                  <div className=" space-x-5 flex items-center">
+                    <div className=" flex justify-start items-center">
+                      <input
+                        type="radio"
+                        id="yes"
+                        name="publish"
+                        value="true"
+                        checked={isPublished === true}
+                        className=" w-4 h-4 mr-1"
+                        onChange={(e) => setIsPublished(e.target.value === 'true')}
+                        />
+                      <label htmlFor="yes">Yes</label>
+                    </div>
+                    <div className=" flex justify-start items-center">
+                      <input
+                        type="radio"
+                        id="no"
+                        name="publish"
+                        value="false"
+                        checked={isPublished === false}
+                        className=" w-4 h-4 mr-1"
+                        onChange={(e) => setIsPublished(e.target.value === 'true')}
+                        />
+                      <label htmlFor="no" className=" text-lg">No</label>
+                    </div>
+                  </div>
+                  {isPublished}
+              <input
+                onChange={(e) => setImg(e.target.value)}
+                type="text"
+                className="rounded p-2 bg-background block m-auto w-full"
+                placeholder="Image Link"
+              />
+              <button
+                onClick={handleSubmit}
+                className="rounded bg-primary-500 p-2 block m-auto my-3 w-full"
+              >
+                Create
+              </button>
+            </div>
+          )}
+        </>
+      );
 }
 
 export default AddCourse;
