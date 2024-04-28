fc8abcd656712d27c7e103da94267ea59816d694
vighnesh-7 <vangarivighnesh@gmail.com>
2024-04-28 16:26:57 +0530

    default route removed

--- src/app/instructor/assignments/[id]/page.tsx
+++ src/app/instructor/assignments/[id]/page.tsx
@@ -13,14 +13,14 @@ export default async function mentorAssignments({ params }: {
   return (
     
     <div className="mx-2 md:mx-6 px-2 md:px-8 py-2 flex flex-col gap-4">
-      <h1 className="text-center text-3xl font-semibold py-2">ASSIGNMENTS</h1>
+      <h1 className="text-center text-3xl font-semibold py-2 max-sm:text-2xl">ASSIGNMENTS</h1>
       {!coursesWithAssignments||coursesWithAssignments.length===0 ? <div className="text-center">No Assignments found!</div> : 
-        <div className=" text-center w-full flex justify-center items-center mt-8" >
+        <div className=" text-center w-full flex justify-center items-center mt-5" >
             <Image 
                 src="https://i.postimg.cc/jSvL48Js/oops-404-error-with-broken-robot-concept-illustration-114360-1932-removebg-preview.png" 
                 alt="404" 
-                width={400}
-                height={400}
+                width={500}
+                height={500}
             />
         </div>
       }
