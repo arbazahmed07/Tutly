8a06652d30d2b70023064ba3ddf2176f530e4f0a
vighnesh-7 <vangarivighnesh@gmail.com>
2024-04-28 19:39:01 +0530

    not submitted

--- src/app/instructor/assignments/[id]/page.tsx
+++ src/app/instructor/assignments/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { getAllAssignedAssignmentsByUserId } from "@/actions/assignments";
-import AllAssignments from "@/components/allAssignments";
+import StudentWiseAssignments from "@/components/StudentWiseAssignments";
 
 
 
@@ -13,7 +13,7 @@ export default async function mentorAssignments({ params }: {
     
     <div className="mx-2 md:mx-6 px-2 md:px-8 py-2 flex flex-col gap-4">
       <h1 className="text-center text-3xl font-semibold py-2">ASSIGNMENTS</h1>
-      {!coursesWithAssignments||coursesWithAssignments.length===0 ? <div className="text-center">No Assignments found!</div> : <AllAssignments courses={courses} assignments={coursesWithAssignments} userId={params.id} />}
+      {!coursesWithAssignments||coursesWithAssignments.length===0 ? <div className="text-center">No Assignments found!</div> : <StudentWiseAssignments courses={courses} assignments={coursesWithAssignments} userId={params.id} />}
     </div>
   );
 }
--- src/components/StudentWiseAssignments.tsx
+++ src/components/StudentWiseAssignments.tsx
@@ -2,11 +2,13 @@
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { MdOutlineSportsScore } from "react-icons/md";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 
 
 
-export default function AllAssignments({ courses, assignments, userId }: any) {
+
+export default function StudentWiseAssignments ({ courses, assignments, userId }: any) {
 const [currentCourse, setCurrentCourse] = useState<string>(courses[0]?.id);
 const [isMounted, setIsMounted] = useState(false);
 const router = useRouter();
@@ -24,6 +26,14 @@ return;
 
 return (
 <div className="flex flex-col gap-4">
+    <div className="relative">
+      <button
+        onClick={() => router.back()}
+        className="absolute -top-10 -left-10  p-2"
+        >
+        <IoMdArrowRoundBack className=" w-8 h-8" />
+      </button>
+    </div>
     <div className="flex flex-wrap justify-between items-center gap-3">
         <div>
                 {courses?.map((course: any) => {
@@ -160,7 +170,7 @@ return (
                                 if (eachSubmission.points.length !== 0 && unreviewed !== 'reviewed' ) {
                                 return (
                                     <div className="flex gap-6 items-center" key={index}>
-                                        <div className="rounded-full p-2.5 bg-yellow-600 hover:bg-yellow-500">Under review</div>
+                                        <div className="rounded-full p-3 bg-yellow-600 hover:bg-yellow-500">Under review</div>
                                     </div>
                                 )
                                 } else if(eachSubmission.points.length !== 0 && unreviewed === 'reviewed') {
@@ -205,6 +215,50 @@ return (
         });
         });
     })
+    }
+    {
+    unreviewed === 'not-submitted' &&
+            assignments.map((course: any) => {
+        if (course.id !== currentCourse) return null;
+        return course.classes.map((cls: any) => {
+            return cls.attachments.map((assignment: any) => {
+            if (assignment.submissions.length > 0) return null;
+            return (
+                <div key={assignment.id} className="border rounded-lg p-1 md:p-3">
+                <div className="flex items-center p-2 md:p-0 md:px-4 justify-around md:justify-between flex-wrap">
+                    <div className="flex md:flex-row items-center justify-between w-full flex-wrap">
+                    <div className="text-sm">
+                        <h2 className="flex-1 font-medium m-2">{assignment.title}</h2>
+                    </div>
+                    <div className="flex gap-3 md:gap-6 items-center text-sm font-medium text-white flex-wrap">
+                        {
+                        assignment.submissions.length === 0 &&
+                        <div className="flex gap-6 itens-center">
+                            <div className="rounded-full p-2 px-3 bg-secondary-600">not submitted</div>
+                        </div>
+                        }
+                        <button
+                        title="btn"
+                        onClick={() => {
+                            if (userId) {
+                            router.push(`/assignments/${assignment.id}?userId=${userId}`);
+                            } else {
+                            router.push(`/assignments/${assignment.id}`);
+                            }
+                        }
+                        }
+                        className="p-2 px-4 bg-blue-500 rounded"
+                        >
+                        View Details
+                        </button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            );
+            });
+        });
+        })
     }
 </div>
 );
