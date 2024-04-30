16b91a96c23523e2ed20e5dffded014cf69cda9c
Kalyan Tingani <kalyantingani@gmail.com>
2024-04-30 21:50:37 +0530

    attendance-to-db

--- src/actions/attendance.ts
+++ src/actions/attendance.ts
@@ -3,18 +3,20 @@ import getCurrentUser from "./getCurrentUser";
 
 export const postAttendance = async ({classId,data}: {
     classId: string;
-    data: { Username: string,Duration:number }[];
+    data: { username: string,Duration:number }[];
 }) => {
     const currentUser = await getCurrentUser();
     if (!currentUser) {
         throw new Error("You must be logged in to attend a class");
     }
+    const parsedData = JSON.parse(JSON.stringify(data))
     const postAttendance = await db.attendance.createMany({
         data: [
-            ...data.map((student:any) => ({
+            ...parsedData.map((student:any) => ({
                 classId,
-                userId: student.Username,
-                attendedDuration:student.Duration
+                username: student.username,
+                attendedDuration:student.Duration,
+                data: student?.Joins
             }))
         ]
     })
