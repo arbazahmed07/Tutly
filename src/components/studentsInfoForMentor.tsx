b48eae25f7f71bb68ac823c3f23db318d3923d95
Uday Sagar <111575806+UdaySagar-Git@users.noreply.github.com>
2024-06-13 00:10:36 +0530

    Revert "responsiveness-stats"

--- src/components/studentsInfoForMentor.tsx
+++ src/components/studentsInfoForMentor.tsx
@@ -5,9 +5,9 @@ export default function StudentsInfoForMentor({ currentUser, mstudents }: any) {
     <div className="flex flex-wrap gap-8 justify-center">
       {mstudents.map((student: any, index: number) => {
         return (
-          <Link key={index} href={`${currentUser?.role === "INSTRUCTOR" ? `/instructor/mentor/student/${student.username}` : `/mentor/student/${student.username}`}`} className="rounded-xl shadow-blue-500 shadow-sm p-2 w-full lg:w-1/4">
-            <div className="py-2 ">
-              <h1 className="text-sm font-medium mb-2">{student.name}</h1>
+          <Link key={index} href={`${currentUser?.role === "INSTRUCTOR" ? `/instructor/mentor/student/${student.username}` : `/mentor/student/${student.username}`}`} className="rounded-xl shadow-blue-500 shadow-sm p-2 w-1/4">
+            <div className="py-2">
+              <h1 className="text-sm font-medium">{student.name}</h1>
               <h1 className="text-xs">@{student.username}</h1>
             </div>
           </Link>
