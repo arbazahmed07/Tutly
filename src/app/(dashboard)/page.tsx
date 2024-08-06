6feabfa740df95539c005839e2797ccf67f4e2be
Uday Sagar <udaysagar.mail@gmail.com>
2024-08-06 12:26:18 +0530

    fix home page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 import React, { useEffect } from 'react';
 import {
   getDashboardData,
@@ -19,46 +19,46 @@ import {
   getEnrolledStudents,
 } from "@/actions/courses";
 import getCurrentUser from "@/actions/getCurrentUser";
-import OneSignal from "react-onesignal";
+// import OneSignal from "react-onesignal";
 export default async function Home() {
 
   
 
-  useEffect(() => {
-    const loadOneSignal = async () => {
-      try {
-        console.log("Loading OneSignal SDK...");
-        await OneSignal.init({
-          appId: process.env.NEXT_PUBLIC_ONE_SIGNAL!,
-          allowLocalhostAsSecureOrigin: true,
-        });
+  // useEffect(() => {
+  //   const loadOneSignal = async () => {
+  //     try {
+  //       console.log("Loading OneSignal SDK...");
+  //       await OneSignal.init({
+  //         appId: process.env.NEXT_PUBLIC_ONE_SIGNAL!,
+  //         allowLocalhostAsSecureOrigin: true,
+  //       });
 
-        console.log("OneSignal has been successfully initialized.");
-        OneSignal.Slidedown.promptPush();
-      } catch (error) {
-        console.error("Error initializing OneSignal:", error);
-      }
-    };
+  //       console.log("OneSignal has been successfully initialized.");
+  //       OneSignal.Slidedown.promptPush();
+  //     } catch (error) {
+  //       console.error("Error initializing OneSignal:", error);
+  //     }
+  //   };
 
-    // Check if OneSignal SDK is already loaded
-    if (!window.OneSignal) {
-      const script = document.createElement('script');
-      script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
-      script.async = true;
-      script.onload = loadOneSignal;
-      script.onerror = () => console.error("Error loading OneSignal SDK script.");
-      document.head.appendChild(script);
-    } else {
-      loadOneSignal();
-    }
+  //   // Check if OneSignal SDK is already loaded
+  //   if (!window.OneSignal) {
+  //     const script = document.createElement('script');
+  //     script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
+  //     script.async = true;
+  //     script.onload = loadOneSignal;
+  //     script.onerror = () => console.error("Error loading OneSignal SDK script.");
+  //     document.head.appendChild(script);
+  //   } else {
+  //     loadOneSignal();
+  //   }
 
-    // Cleanup function
-    return () => {
-      console.log("Cleaning up OneSignal...");
-      // Optionally, you can call OneSignal.logout() if you need to clear the user session
-      // OneSignal.logout();
-    };
-  }, []);
+  //   // Cleanup function
+  //   return () => {
+  //     console.log("Cleaning up OneSignal...");
+  //     // Optionally, you can call OneSignal.logout() if you need to clear the user session
+  //     // OneSignal.logout();
+  //   };
+  // }, []);
 
 
   const currentUser = await getCurrentUser();
