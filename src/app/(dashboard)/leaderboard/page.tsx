4276d3fe36d751769b0d93231ab0e7c9e98db0e9
Kalyan <kalyantingani@gmail.com>
2024-08-07 18:13:24 +0530

    student-leaderboard-fix

--- src/app/(dashboard)/leaderboard/page.tsx
+++ src/app/(dashboard)/leaderboard/page.tsx
@@ -1,17 +1,22 @@
+import { getAttendanceForLeaderbaord } from "@/actions/attendance";
+import { totalNumberOfClasses } from "@/actions/classes";
 import getCurrentUser from "@/actions/getCurrentUser";
-import getLeaderboardData from "@/actions/getLeaderboard";
+import getLeaderboardData, { getSubmissionsCountOfAllStudents } from "@/actions/getLeaderboard";
 import Leaderboard from "@/components/leaderBoard";
 
 export default async function Page() {
   const data: any = await getLeaderboardData();
   const currentUser = await getCurrentUser();
+  const noOfSubmissions = await getSubmissionsCountOfAllStudents();
+  const attendance = await getAttendanceForLeaderbaord();
+  const totalClasses = await totalNumberOfClasses();
 
   if (data && data.sortedSubmissions && data.enrolledCourses) {
 
     const { sortedSubmissions, enrolledCourses } = data;
 
     return (
-      <Leaderboard submissions={sortedSubmissions} courses={enrolledCourses} currentUser={currentUser}/>
+      <Leaderboard submissions={sortedSubmissions} courses={enrolledCourses} currentUser={currentUser} noOfSubmissions={noOfSubmissions} attendance={attendance} totalClasses={totalClasses}/>
     );
 
   } else {
