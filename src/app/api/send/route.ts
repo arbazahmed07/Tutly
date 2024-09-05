4afadd6f269fe000daee5820210356ebd539694d
Uday Sagar <udaysagar.mail@gmail.com>
2024-09-05 19:39:48 +0530

    email notification

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,22 @@
+import { sendEmail } from "@/actions/resend"
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  try {
+    const email = body.email;
+
+    let ip = req.headers.get("x-forwarded-for") || req.headers.get("x-real-ip");
+    
+    const device = req.headers.get("user-agent");
+
+    const { data, error } = await sendEmail({ email, ip, device });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
