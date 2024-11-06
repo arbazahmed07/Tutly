e97b0cea6572f0885a6ab2880ecd05a561f1aee2
Kalyan <kalyantingani@gmail.com>
2024-11-06 14:58:18 +0530

    mentor-leaderboard

--- src/app/mentor/leaderboard/page.tsx
+++ src/app/mentor/leaderboard/page.tsx
@@ -3,17 +3,17 @@ import Leaderboard from "@/components/leaderBoard";
 
 export default async function mentorLeaderboard() {
   const data: any = await getMentorLeaderboardData();
-  if (data?.sortedSubmissions && data.createdCourses) {
-    const { sortedSubmissions, createdCourses } = data;
+  if (data?.sortedSubmissions && data.enrolledCourses) {
+    const { sortedSubmissions, enrolledCourses } = data;
     
     return (
       <Leaderboard
         submissions={sortedSubmissions}
-        courses={createdCourses}
+        courses={enrolledCourses}
         currentUser={data.currentUser}
       />
     );
   } else {
-    return null;
+    return <div>No course enrolled</div>
   }
 }
