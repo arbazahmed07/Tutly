c721baf0a203a190f5f2068232f83096733b397d
Uday Sagar <udaysagar.mail@gmail.com>
2024-11-07 00:09:21 +0530

    initial playgrounds

---
+++ src/pages/playgrounds/_components/MonacoEditor.tsx
@@ -0,0 +1,36 @@
+import Editor from "@monaco-editor/react";
+import {
+  useActiveCode,
+  SandpackStack,
+  FileTabs,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
+
+export default function MonacoEditor() {
+  const { code, updateCode } = useActiveCode();
+  const { sandpack } = useSandpack();
+  const languageMap: Record<string, string> = {
+    html: "html",
+    css: "css",
+    js: "javascript",
+  };
+
+  return (
+    <SandpackStack className="margin-0 h-[95vh]">
+      <FileTabs closableTabs />
+      <div className="h-screen flex-1 pt-2">
+        <Editor
+          width="100%"
+          height="100%"
+          language={
+            languageMap[sandpack.activeFile.split(".")[1] as string] || "plaintext"
+          }
+          theme="vs-light"
+          key={sandpack.activeFile}
+          value={code}
+          onChange={(value) => updateCode(value || "")}
+        />
+      </div>
+    </SandpackStack>
+  );
+}
---
+++ src/pages/playgrounds/react/ReactPlayground.tsx
@@ -0,0 +1,32 @@
+import { Suspense, useState } from "react";
+import FolderUpload from "./FileUpload";
+import Playground from "../_components/Playground";
+
+const ReactPlayground = ({ currentUser }: { currentUser: any }) => {
+  const [filesObj, setFilesObj] = useState<Record<string, string> | undefined>(
+    undefined,
+  );
+
+  return (
+    <>
+      {!filesObj ? (
+        <>
+          <h1 className="mt-6 text-center text-2xl font-bold text-orange-300">
+            React Playground
+          </h1>
+          <FolderUpload setFilesObj={setFilesObj} />
+        </>
+      ) : (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Playground
+            currentUser={currentUser}
+            initialFiles={filesObj}
+            template="react"
+          />
+        </Suspense>
+      )}
+    </>
+  );
+};
+
+export default ReactPlayground;
