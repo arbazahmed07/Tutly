ecdd74d8b3acaae89e31b6b8d44b9bd0db4b1743
Uday <udaysagar.mail@gmail.com>
2025-02-04 22:54:29 +0530

    remove astro

--- src/utils/getGreeting.ts
+++
@@ -1,8 +0,0 @@
-export const getGreeting = () => {
-  const currentIST = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
-  const hour = currentIST.getHours();
-
-  if (hour < 12) return "Good morning";
-  if (hour < 18) return "Good afternoon";
-  return "Good evening";
-};
